8b976ef15ec05b54da44462bef8a8068c498d975
abhay-varnekar <silverwingvaghar@gmail.com>
2026-07-20 12:03:17 +0530

    Make background blobs fluid and responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,13 +20,13 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Plus+Jakarta+Sans:wght@500;600;700;800&display=swap" rel="stylesheet" />
       </head>
-      <body className="antialiased bg-grain">
+      <body className="antialiased bg-grain overflow-x-hidden w-full">
         <div className="flex min-h-screen">
           <Sidebar />
           <div className="flex-1 md:ml-[320px] flex flex-col min-h-screen relative">
             {/* Background Blobs */}
-            <div className="blob blob-violet w-[500px] h-[500px] top-[-10%] left-[-10%]" />
-            <div className="blob blob-sky w-[400px] h-[400px] top-[20%] right-[-5%]" />
+            <div className="blob blob-violet w-[80vw] max-w-[500px] aspect-square top-[-10%] left-[-10%]" />
+            <div className="blob blob-sky w-[60vw] max-w-[400px] aspect-square top-[20%] right-[-5%]" />
             
             <TopBar />
             <main className="flex-1 p-0 md:p-8 relative z-10">
